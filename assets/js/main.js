5b5bf0da878c7840fbd0bcd5b7ad06097a317b8a
Wassim-RF <wassimrifi0313@gmail.com>
2025-10-31 12:17:59 +0100

    J'ai commencai la dernier section de resultat et j'ai terminer les calcule

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,43 +7,57 @@ let buttoonOfProfessionPressed = false;
 
 
 // Calculer le versement mentionnel
+
 let mentantRange = document.getElementById("mentant_range");
 let numberRangeMentant = document.getElementById("number_range_mentant");
 let dureeRange = document.getElementById("duree_range");
 let numberRangeDuree = document.getElementById("number_range_duree");
+let salairRange = document.getElementById("Salair_range");
+let numberRangeSalair = document.getElementById("salair_range_mentant");
 let TEG; // taux nominal annuel
 let VerMen; 
 let Mentant = mentantRange.value;
 let Duree = dureeRange.value;
+let Salair = salairRange.value;
 
 // calculer le mentant de credit
 numberRangeMentant.addEventListener("input" , function() {
     mentantRange.value = numberRangeMentant.value;
     Mentant = mentantRange.value;
+    console.log(Mentant);
 });
 mentantRange.addEventListener("input" , function () {
     Mentant = mentantRange.value;
     numberRangeMentant.value = mentantRange.value;
+    console.log(Mentant);
 });
 // calculer la duree
 numberRangeDuree.addEventListener("input" , function() {
     dureeRange.value = numberRangeDuree.value;
     Duree = dureeRange.value;
-    console.log(Duree)
+    console.log(Duree);
 });
 dureeRange.addEventListener("input" , function () {
     Duree = dureeRange.value;
     numberRangeDuree.value = dureeRange.value;
-    console.log(Duree)
+    console.log(Duree);
+});
+// calculer le salair
+numberRangeSalair.addEventListener("input" , function() {
+    salairRange.value = numberRangeSalair.value;
+    Salair = salairRange.value;
+    console.log(Duree);
+});
+salairRange.addEventListener("input" , function () {
+    Salair = salairRange.value;
+    numberRangeSalair.value = salairRange.value;
+    console.log(Salair);
 });
 
-TEG = TEG/12;
-VerMen = Mentant * ((TEG * ((1 + TEG) ** Duree)) / (((1 + TEG) ** Duree) - 1));
 
 
 
-
-
+// stocker la valeur de TEG
 buttonProfessionFonctionnaire.addEventListener("click" , () => {
     buttonProfessionFonctionnaire.classList.add("button_profession_afterClicked");
     buttonProfessionSalarie.classList.remove("button_profession_afterClicked");
@@ -54,6 +68,7 @@ buttonProfessionFonctionnaire.addEventListener("click" , () => {
 
 
     TEG = 0.08;
+    TEG = TEG/12;
     console.log(TEG);
 });
 
@@ -67,6 +82,7 @@ buttonProfessionSalarie.addEventListener("click" , () => {
 
 
     TEG = 0.1;
+    TEG = TEG/12;
     console.log(TEG);
 });
 
@@ -80,6 +96,7 @@ buttonProfessionRetraite.addEventListener("click" , () => {
 
 
     TEG = 0.08;
+    TEG = TEG/12;
     console.log(TEG);
 });
 
@@ -93,6 +110,7 @@ buttonProfessionProfessionnel.addEventListener("click" , () => {
 
 
     TEG = 0.11;
+    TEG = TEG/12;
     console.log(TEG);
 });
 
@@ -125,10 +143,13 @@ let continueProfessionButton = document.getElementById("continueProfessionButton
 
 // continue au section de selection de info
 let continuetoInfoSectionBUtton = document.getElementById("toInfoSectionBUtton").addEventListener("click" , () => {
-    if (Mentant !== "0") {
+    if (Mentant !== "0" && Duree !== "0") {
         let select_mentantDure_section = document.getElementById("select_mentant&durre_section").style.display = "none";
         let select_info_section = document.getElementById("select_info_section").style.display = "flex";
         console.log(Mentant);
+        console.log(Duree);
+        VerMen = Mentant * (TEG * ((1 + TEG) ** Duree)) / (((1 + TEG) ** Duree) - 1);
+        console.log(VerMen)
     } else {
         alert("Vous avez lesse votre menttant est duree vide")
         console.log(Mentant);
